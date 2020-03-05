df351fc65b38c76f1524e7d7d5bf740946ded0e1
andrei8055 <agape.andrei93@yahoo.com>
2020-03-05 15:57:23 +0100

    assigment 15

---
+++ Assignment-15/assigment15.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+  int x = 15;
+  int *ptr = &x;
+
+  printf("The value of int variable is: %d\n", x);
+  printf("The value of the pointer to the int variable is: %d\n", ptr);
+  printf("The memory address of the int variable is: %d\n", &x);
+  printf("The value held at the memory location that the pointer is pointing to is: %d\n", *ptr);
+return 1;
+}
