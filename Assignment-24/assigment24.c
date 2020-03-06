e47c0a8295b296fcb0b6ecbd2a025af5fdabdc5b
andrei8055 <agape.andrei93@yahoo.com>
2020-03-06 11:10:56 -0800

    assigment 24

---
+++ Assignment-24/assigment24.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<fcntl.h>
+
+/*
+Permissions
+
+S_IRWXU 0000700
+S_IRUSR 0000400
+S_IWUSR 0000200
+S_IXUSR 0000100
+
+S_IRWXG 0000070
+S_IRGRP 0000040
+S_IWGRP 0000020
+S_IXGRP 0000010
+
+S_IRWXO 0000007
+S_IROTH 0000004
+S_IWOTH 0000002
+S_IXOTH 0000001
+
+S_ISUID 0004000
+S_ISGID 0002000
+S_ISVTX 0001000
+*/
+
+int main(){
+    int filedescriptor;
+
+    //Open file for read+write, create if not exist with permissions 0700
+    filedescriptor = open("test.txt", O_RDWR | O_CREAT, S_IRWXU);
+
+    if(filedescriptor < 0)
+        printf("`open()` failed");
+    else
+        printf("`open()` succeed");
+
+    return 1;
+}
